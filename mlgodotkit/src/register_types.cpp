11f3124bba2abf6249e03ef8874903e487e443af
Hunter Paxton <hunter.paxton@mines.sdsmt.edu>
2025-11-01 21:32:42 -0600

    Initial commit of Linalg base class for eigen wrapper

--- mlgodotkit/src/register_types.cpp
+++ mlgodotkit/src/register_types.cpp
@@ -3,35 +3,30 @@
 using namespace godot;
 
 void initialize_mlgodotkit_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
         return;
-    }
 
     GDREGISTER_CLASS(NNNode);
     GDREGISTER_CLASS(LRNode);
     GDREGISTER_CLASS(DTreeNode);
     GDREGISTER_CLASS(Benchmark);
+    GDREGISTER_CLASS(Linalg);
 }
 
 void uninitialize_mlgodotkit_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
         return;
-    }
 }
 
 extern "C" {
-    // Initialization.
     GDExtensionBool GDE_EXPORT mlgodotkit_library_init(
-        GDExtensionInterfaceGetProcAddress p_get_proc_address,
-        const GDExtensionClassLibraryPtr p_library,
-        GDExtensionInitialization *r_initialization)
-    {
+            GDExtensionInterfaceGetProcAddress p_get_proc_address,
+            const GDExtensionClassLibraryPtr p_library,
+            GDExtensionInitialization *r_initialization) {
         godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-
         init_obj.register_initializer(initialize_mlgodotkit_module);
         init_obj.register_terminator(uninitialize_mlgodotkit_module);
         init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
         return init_obj.init();
     }
 }
